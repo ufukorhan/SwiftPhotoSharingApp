ed9c4edeeff69af780f2028f5ed3ddb3be0938b0
Ufuk ORHAN <ufuk.orhan@logsign.net>
2022-05-28 17:58:27 +0300

    feat: add upload interface

--- PhotoSharingApp/UploadViewController.swift
+++ PhotoSharingApp/UploadViewController.swift
@@ -7,15 +7,38 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
-
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var yorumTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func gorselSec() {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
 
-
+    @IBAction func uploadButtonTiklandi(_ sender: Any) {
+    }
+    
 
 }
